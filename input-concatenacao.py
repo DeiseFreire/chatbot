4c0bc30ba77873b1d88c295641f577ec38f2ccd9
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-09-20 15:00:41 -0300

    Create input-concatenacao.py

---
+++ input-concatenacao.py
@@ -0,0 +1,31 @@
+# -------------------------------------------------------------------------------------------
+# Fonte da ideia
+# Aula 2 - Python chatbot - input concatenação
+# https://www.youtube.com/watch?v=KPL0-g67xZ8&list=PLsMpSZTgkF5Bdh7-o3VnbpRpahOeDJONh&index=2
+# --------------------------------------------------------------------------------------------
+
+‘’’
+>>> print("Olá mundo")
+Olá mundo
+>>> input()
+algum texto
+'algum texto'
+>>> texto = input()
+algum texto
+>>> texto
+'algum texto'
+>>> input('Pode falar: ')
+Pode falar: fala alguma coisa 
+'fala alguma coisa'
+>>> 1+4
+5
+>>> "ashdih"+"UIASHIUDHS" 
+'ashdihUIASHIUDS'
+>>> 
+‘’’
+print('Olá, qual o seu nome?')
+nome = input('>: ')
+print('Muito prazer') 
+$ vim chatbot.py
+$ python chatbot.py
+
