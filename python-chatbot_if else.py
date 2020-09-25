2624340b7262dcbb27ac83b37f3f4b1d257cae52
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-09-25 14:15:54 -0300

    Create python-chatbot_if else.py

---
+++ python-chatbot_if else.py
@@ -0,0 +1,34 @@
+# -------------------------------------------------------------------------------------------
+# Fonte da ideia: 
+# Aula 3 - Python chatbot - if else
+# https://www.youtube.com/watch?v=IZ-KyTuUCXU&list=PLsMpSZTgkF5Bdh7-o3VnbpRpahOeDJONh&index=3
+# -------------------------------------------------------------------------------------------
+
+>>> a = 2
+>>> a == 2
+True
+>>> a == 3
+False
+>>> if a == 2:
+'''    print('a = 2')
+'''
+a = 2
+>>> a = 3
+>>> a
+3
+>>> if a == 2:
+''' print('a = 2')
+'''
+>>> if a == 2:
+'''    print('a = 2')
+'''
+>>> if a == 2:
+'''    print('a = 2')
+''' else:
+'''    print('a não é 2')
+'''
+a não é 2
+>>> a = '2'
+>>> a == 2 
+False
+>>>
