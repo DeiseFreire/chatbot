fb57c090074cc8da33e26d27a26a1503a6105980
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-10-08 10:55:08 -0300

    Update loop-com-while-e-break.py

--- loop-com-while-e-break.py
+++ loop-com-while-e-break.py
@@ -4,19 +4,21 @@
 # https://www.youtube.com/watch?v=UQORiDqVmBA&list=PLsMpSZTgkF5Bdh7-o3VnbpRpahOeDJONh&index=5
 # -------------------------------------------------------------------------------------------
 
-print ('Olá, qual o seu nome?')
-nome=input('>: ')
-if 'O meu nome eh ' in nome:
+print ('Olá, qual o seu nome?') # impressão
+nome=input('>: ') # entrada
+if 'O meu nome eh ' in nome: # e se ou se 
 nome=nome[14:]
-if nome=='Raimundo':
+if nome=='Raimundo': # e se ou se
 print(Eaew '+nome) # impressão
 else: # outro
 print('Muito prazer '+nome)
->>> while 2==2:
-... print(' ')
+>>> while 2==2: # enquanto
+... print(' ') # impressão
 Traceback (most recent call last): # Rastrear de volta (última chamada mais recente):
 File "<stdin>", line 2, i # Arquivo "<stdin>", linha 2, i
 n <module> # módulo
 keyboardInterrupt # interrupção do teclado
 >>> n = 1
->>> while # enquanto
+>>> while n < 3: # enquanto
+... print(n)  # impressão     
+... n = n + 1 
