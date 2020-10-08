d0502a54928cce1c95a4c78fa7ea4c5c81abcffb
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-10-08 07:49:14 -0300

    Create loop-com-while-e-break.py

---
+++ loop-com-while-e-break.py
@@ -0,0 +1,16 @@
+# -------------------------------------------------------------------------------------------
+# Fonte da ideia: 
+# Aula 5 - Python chatbot - Loops com while e break
+# https://www.youtube.com/watch?v=UQORiDqVmBA&list=PLsMpSZTgkF5Bdh7-o3VnbpRpahOeDJONh&index=5
+# -------------------------------------------------------------------------------------------
+
+print ('Olá, qual o seu nome?')
+nome=input('>: ')
+if 'O meu nome eh ' in nome:
+nome=nome[14:]
+if nome=='Raimundo':
+print(Eaew '+nome)
+else:
+print('Muito prazer '+nome)
+>>> while 2==2:
+... print(' ')
