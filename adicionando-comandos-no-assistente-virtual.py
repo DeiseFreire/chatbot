8670ffb52f37d568de5d4228c4c9bff063b27aca
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-07 09:02:57 -0300

    Update adicionando-comandos-no-assistente-virtual.py

--- adicionando-comandos-no-assistente-virtual.py
+++ adicionando-comandos-no-assistente-virtual.py
@@ -9,78 +9,107 @@ from chatterbot import ChatBot
 import os
 import speech_recognition as sr
 import pyttsx3
-speaker=pyttsx3.init()
-bot=ChatBot('Jarvis', read_only=True)
-dict_cmds={}
+
+speaker = pyttsx3.init()
+bot = ChatBot( 'Jarvis', read_only=True )
+dict_cmds = {}
+
+
 def load_cmds():
-lines=open('cmds.txt','r').readlines()
+
+
+    lines = open( 'cmds.txt', 'r' ).readlines()
 for line in lines:
-line=line.replace('\n','')
-parts=line.split('\t')
-dict_cmds.update({parts[0]:parts[1]}) 
+    line = line.replace( '\n', '' )
+parts = line.split( '\t' )
+dict_cmds.update( {parts[0]: parts[1]} )
+
+
 def setVoice():
-voices=speaker.getProperty('voices')
+
+
+    voices = speaker.getProperty( 'voices' )
 for voice in voices:
-if voice.name=='brazil':
-speaker.setProperty('voice',voice.id)
+    if voice.name == 'brazil':
+    speaker.setProperty( 'voice', voice.id )
+
+
 def speak(text):
-speaker.say(text)
+
+
+    speaker.say( text )
 speaker.runAndWait()
-def evaluate(text): # passar o comando valor
-result=None
+
+
+def evaluate(text):  # passar o comando valor
+
+
+    result = None
 try:
-result=dict_cmds[text] # assina o result = tipo de comando
+    result = dict_cmds[text]  # assina o result = tipo de comando
 except:
-result=None
+result = None
 return result
+
+
 def run_cmd(cmd_type):
-result=None
+
+
+    result = None
 if cmd_type == 'asktime':
-now=datetime.now()
-result='São' + now.hour + 'horas e' + now.minute + 'minutos.'
-result='São' + now.hour + 'horas e' + now.minute + 'minutos.'
-elif cmd_type=='askdate':
-now=datetime.now()
-result='Hoje é' + now.day + 'de' + now.month
+    now = datetime.now()
+result = 'São' + now.hour + 'horas e' + now.minute + 'minutos.'
+result = 'São' + now.hour + 'horas e' + now.minute + 'minutos.'
+elif cmd_type == 'askdate':
+now = datetime.now()
+result = 'Hoje é' + now.day + 'de' + now.month
 else
-result = None  
+result = None
 return result
-setVoice() # setar a voz
-load_cmds() # carregar comandos 
-for k,v in dict_cmds.items():
-print(k, '=====>', v)
+setVoice()  # setar a voz
+load_cmds()  # carregar comandos 
+for k, v in dict_cmds.items():
+    print( k, '=====>', v )
 from pocketsphinx import pocketsphinx, Jsgf, FsgModel
+
 # create decoder object
-config=pocketsphinx.Decoder.default_config()
-config.set_string("-hmm",'model') # set the path of the hidden Markov model (HMM) parameter files
-config.set_string("-lm",'model.lm.bin')
-config.set_string("-dict",'model.dic')
-config.set_string("-logfn",os.devnull) # disable logging (logging causes unwanted output in terminal)
-decoder=pocketsphinx.Decoder(config)
+config = pocketsphinx.Decoder.default_config()
+config.set_string( "-hmm", 'model' )  # set the path of the hidden Markov model (HMM) parameter files
+config.set_string( "-lm", 'model.lm.bin' )
+config.set_string( "-dict", 'model.dic' )
+config.set_string( "-logfn", os.devnull )  # disable logging (logging causes unwanted output in terminal)
+decoder = pocketsphinx.Decoder( config )
+
+
 def recognize_pt(audio):
-raw_data=audio.get_raw_data(convert_rate=16000,convert_width=2)
-decoder.star_utt() # begin utterance processing
-decoder.process_raw(raw_data,False,True) # process audio data width recognition enabled (no_search=False), as a full utterance (full_utt=True)
-decoder.end_utt() #stop utterance processing
-hypothesis=decoder.hyp()
+
+
+    raw_data = audio.get_raw_data( convert_rate=16000, convert_width=2 )
+decoder.star_utt()  # begin utterance processing
+decoder.process_raw( raw_data, False,
+                     True )  # process audio data width recognition enabled (no_search=False), as a full utterance (full_utt=True)
+decoder.end_utt()  # stop utterance processing
+hypothesis = decoder.hyp()
 if hypothesis is not None:
-return hypothesis.hypstr
+    return hypothesis.hypstr
 return None
-r=sr.Recognizer()
+r = sr.Recognizer()
 import traceback
+
 with sr.Microphone() as s:
-r.adjust_for_ambient_noise(s)
+    r.adjust_for_ambient_noise( s )
 while True:
-try:
-audio=r.listen(s)
-speech=recognize_pt(audio) # usando o pocketsphinx
-print('Você disse: ',speech)
-response=bot.get_response(speech)
-print('Bot: ',response)
-speak(response)
-print('Tipo de comando: ',evaluate(speech))
+    try:
+    audio = r.listen( s )
+speech = recognize_pt( audio )  # usando o pocketsphinx
+print( 'Você disse: ', speech )
+response = bot.get_response( speech )
+print( 'Bot: ', response )
+speak( response )
+print( 'Tipo de comando: ', evaluate( speech ) )
 except:
-print('Algum erro ocorreu.') 
+print( 'Algum erro ocorreu.' )
+ 
 
 
 
