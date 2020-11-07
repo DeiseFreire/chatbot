686ce8d702b58b2429b8eb42fe43d789528ccf0f
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-07 09:00:25 -0300

    Update loops-com-for-e-listas.py

--- loops-com-for-e-listas.py
+++ loops-com-for-e-listas.py
@@ -4,22 +4,19 @@
 # https://www.youtube.com/watch?v=PCo5X2jfIzg&list=PLsMpSZTgkF5Bdh7-o3VnbpRpahOeDJONh&index=7
 # -------------------------------------------------------------------------------------------
 
-print('Olá, qual o seu nome?')
-nome=input('>: ')
-if 'O meu nome eh ' in nome:
-nome=nome[14:]
-if nome=='Raimundo':
-print(Eaew '+nome)
-else:
-print('Muito prazer '+nome)
-if nome=='Will':
-print('Eaew '+nome)
-else:
-print('Muito prazer '+nome)
+print( 'Olá, qual o seu nome?' )
+nome = input( '>: ' )
+if 'O meu nome eh' in nome;
+conhecidos = ['Raimundo', 'Will', 'João']
+for conhecido in conhecidos:
+    if nome == 'Raimundo':
+    print( 'Eaew ' + nome )
+# else:
+print( 'Muito prazer ' + nome )
 while True:
-resposta=input('>: ')
-if resposta=='tchau':
-break
+    resposta = input( '>: ' )
+if resposta == 'tchau':
+    break
 else:
-print('Digite outra coisa')
-print('Tchau tchau!')
+print( 'Digite outra coisa' )
+print( 'Tchau tchau!' )
