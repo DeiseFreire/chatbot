615012b9bc7590127b9171b32c7d234985a5330d
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-07 14:51:18 -0300

    Update loop-com-while-e-break.py

--- loop-com-while-e-break.py
+++ loop-com-while-e-break.py
@@ -4,30 +4,21 @@
 # https://www.youtube.com/watch?v=UQORiDqVmBA&list=PLsMpSZTgkF5Bdh7-o3VnbpRpahOeDJONh&index=5
 # -------------------------------------------------------------------------------------------
 
-print ('Olá, qual o seu nome?') # impressão
-nome=input('>: ') # entrada
-if 'O meu nome eh ' in nome: # e se ou se 
+print ('Olá, qual o seu nome?')
+nome=input('>: ') 
+if 'O meu nome eh ' in nome: 
 nome=nome[14:]
-if nome=='Raimundo': # e se ou se
-print(Eaew '+nome) # impressão
-else: # outro
+if nome=='Raimundo': 
+print(Eaew '+nome) 
+else: 
 print('Muito prazer '+nome)
-# while 2==2: # enquanto
-# print(' ') # impressão
-while True: # enquanto for verdade
-resposta = input ('>: ') # entrada
-if resposta == 'tchau': # e se ou se
-break # parar a execução 
-else: # outro
+# while 2==2: 
+# print(' ') 
+while True: 
+resposta = input ('>: ') 
+if resposta == 'tchau': 
+break 
+else: 
 print('Digite outra coisa') # impressão
 print('Tchau tchau')
-Traceback (most recent call last): # Rastrear de volta (última chamada mais recente):
-File "<stdin>", line 2, i # Arquivo "<stdin>", linha 2, i
-n <module> # módulo
-keyboardInterrupt # interrupção do teclado
-n = 1 # variável recebe 1
-while n < 3: # enquanto
-print(n)  # impressão     
-n = n + 1 # variável recebe 1 + n
 
-      
