a363da301a7dc82547b560728fa0d45616848749
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-06 23:07:13 -0300

    Update Python-chatbot_slicing-in.py

--- Python-chatbot_slicing-in.py
+++ Python-chatbot_slicing-in.py
@@ -4,39 +4,6 @@
 # https://www.youtube.com/watch?v=OVH-mCsP080&list=PLsMpSZTgkF5Bdh7-o3VnbpRpahOeDJONh&index=4
 # -------------------------------------------------------------------------------------------
 
-'''
-[w1lls@Matrix ~] $ vim chatbot.py
-[w1lls@Matrix ~] $ python chatbot.py
-Olá, qual o seu nome?
->: O meu nome é Will
-Muito prazer O meu nome é Will
-[w1lls@Matrix ~] $ 
-'''
-'''
->>> r[0]
-'O'
->>> r[1] 
-' '
->>> r[2]
-'m'
->>> r[-1]
-'l'
->>> r[-2]
-'l'
->>> r[-3]
-'i'
->>> r[0:4]
-'O me'
->>> r[0:5]
-'O meu'
->>> r[5]
-' '
->>> r[14:]
-'Will'
->>> r[:14]
-'O meu nome é  '
->>> '''
-
 print( 'Olá, qual o seu nome?' )
 nome = input( '>: ' )
 if 'O meu nome é ' in nome:
@@ -46,23 +13,3 @@ if nome == 'Raimundo':
 else:
 print( 'Muito prazer ' + nome )
 
-'''
-w1lls@Matrix ~] $ vim chatbot.py
-[w1lls@Matrix ~] $ python chatbot.py
-Olá, qual o seu nome?
->: O meu nome é Will
-Muito prazer O meu nome é Will
-w1lls@Matrix ~] $ vim chatbot.py
-[w1lls@Matrix ~] $ python chatbot.py 
-Olá, qual o seu nome?
->: O meu nome é Will
-Muito prazer Will
-[w1lls@Matrix ~] $ python chatbot.py
-Olá, qual o seu nome?
-Will
-Muito prazer Will
-[w1lls@Matrix ~] $ python chatbot.py
-Olá, qual o seu nome?
->: O meu nome é Jonasvildo
-Muito prazer Jonasvildo
-[w1lls@Matrix ~] $'''
